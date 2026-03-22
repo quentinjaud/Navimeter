bfeb4c58529167f662ce5742c501971bad745a44
quentinjaud <quentin@jaud.me>
2026-03-22 09:02:51 +0100

    feat: calcul polylineSimplifiee a l'import (RDP 50-100 points)
    Fixe aussi force-dynamic manquant sur la page d'accueil (build cassé).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import Link from "next/link";
 import { obtenirSession } from "@/lib/session";
 
+export const dynamic = "force-dynamic";
+
 export default async function PageAccueil() {
   const session = await obtenirSession();
 
